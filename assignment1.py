d15ddb95ea9529970d2dbad0aef0513130f3701e
Harsha Praneeth D <72025415+HarshaPraneeth8@users.noreply.github.com>
2021-05-06 09:30:31 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,2 +1,20 @@
-print('hello, world')
-print('yo')
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+#ignore SSL certificate errors
+ctx=ssl.create_default_context()
+ctx.check_hostname=False
+ctx.verify_mode= ssl.CERT_NONE
+
+url= input('Enter- ')
+html=urllib.request.urlopen(url, context=ctx).read()
+soup=BeautifulSoup(html,'html.parser')
+
+sum=0
+tags=soup('span')
+
+for tag in tags:
+    sum=sum+int(tag.contents[0])
+
+print(sum)
